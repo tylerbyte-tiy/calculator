da73b19c567abbdb103b4ea259432ae87fc715c8
Tyler Hamlett <tylerbyte@users.noreply.github.com>
2017-03-14 22:35:29 -0600

    feat (docs) Assigned numbers to divs in html

--- main.js
+++ main.js
@@ -32,4 +32,71 @@ function modulo (num1m, num2m) {
   return remainder;
 }
 console.assert(modulo(10,3) === 1)
-// document.querySelector('#zero').innerHTML(0)
+
+// document.querySelector('#zero').innerHTML = 0;
+
+var buttonOne = document.getElementById('one')
+
+buttonOne.addEventListener('click', function (){
+  console.log(1);
+});
+
+var buttonTwo = document.getElementById('two')
+
+buttonTwo.addEventListener('click', function (){
+  console.log(2);
+});
+
+var buttonThree = document.getElementById('three')
+
+buttonThree.addEventListener('click', function (){
+  console.log(3);
+});
+
+var buttonFour = document.getElementById('four')
+
+buttonFour.addEventListener('click', function (){
+  console.log(4);
+});
+
+var buttonFive = document.getElementById('five')
+
+buttonFive.addEventListener('click', function (){
+  console.log(5);
+});
+
+var buttonSix = document.getElementById('six')
+
+buttonSix.addEventListener('click', function (){
+  console.log(6);
+});
+
+var buttonSeven = document.getElementById('seven')
+
+buttonSeven.addEventListener('click', function (){
+  console.log(7);
+});
+
+var buttonEight = document.getElementById('eight')
+
+buttonEight.addEventListener('click', function (){
+  console.log(8);
+});
+
+var buttonNine = document.getElementById('nine')
+
+buttonNine.addEventListener('click', function (){
+  console.log(9);
+});
+
+var buttonZero = document.getElementById('zero')
+
+buttonZero.addEventListener('click', function (){
+  console.log(0);
+});
+
+// var buttonPlus = document.getElementById('plus')
+//
+// buttonPlus.addEventListener('click', function (){
+//   console.log(???);
+// });
