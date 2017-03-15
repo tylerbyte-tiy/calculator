b89d327fd8c9f8e7429ec4de4b4cb95a66b4a469
Tyler Hamlett <tylerbyte@users.noreply.github.com>
2017-03-14 22:52:28 -0600

    feat (main) Add result box in html and css

---
+++ buttons.js
@@ -0,0 +1,97 @@
+var buttonOne = document.getElementById('one')
+
+buttonOne.addEventListener('click', function (){
+  console.log(1);
+});
+
+var buttonTwo = document.getElementById('two')
+
+buttonTwo.addEventListener('click', function (){
+  console.log(2);
+});
+
+var buttonThree = document.getElementById('three')
+
+buttonThree.addEventListener('click', function (){
+  console.log(3);
+});
+
+var buttonFour = document.getElementById('four')
+
+buttonFour.addEventListener('click', function (){
+  console.log(4);
+});
+
+var buttonFive = document.getElementById('five')
+
+buttonFive.addEventListener('click', function (){
+  console.log(5);
+});
+
+var buttonSix = document.getElementById('six')
+
+buttonSix.addEventListener('click', function (){
+  console.log(6);
+});
+
+var buttonSeven = document.getElementById('seven')
+
+buttonSeven.addEventListener('click', function (){
+  console.log(7);
+});
+
+var buttonEight = document.getElementById('eight')
+
+buttonEight.addEventListener('click', function (){
+  console.log(8);
+});
+
+var buttonNine = document.getElementById('nine')
+
+buttonNine.addEventListener('click', function (){
+  console.log(9);
+});
+
+var buttonZero = document.getElementById('zero')
+
+buttonZero.addEventListener('click', function (){
+  console.log(0);
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var buttonPlus = document.getElementById('plus')
+
+buttonPlus.addEventListener('click', function (){
+  console.log('+');
+});
+
+var buttonMinus = document.getElementById('minus')
+
+buttonMinus.addEventListener('click', function (){
+  console.log('-');
+});
+
+var buttonMultiply = document.getElementById('multiply')
+
+buttonMultiply.addEventListener('click', function (){
+  console.log('*');
+});
+
+var buttonDivide = document.getElementById('divide')
+
+buttonDivide.addEventListener('click', function (){
+  console.log('/');
+});
+
+var buttonModulo = document.getElementById('modulo')
+
+buttonModulo.addEventListener('click', function (){
+  console.log('%');
+});
+
+var buttonEquals = document.getElementById('equals')
+
+buttonEquals.addEventListener('click', function (){
+  console.log('=');
+});
