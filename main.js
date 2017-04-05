de81173cd1cd6710919a30560cb765bc8a849db3
Tyler Hamlett <tylerbyte@users.noreply.github.com>
2017-04-05 14:53:58 -0600

    feat (main) Convert all numbers/operators to strings

--- main.js
+++ main.js
@@ -25,4 +25,26 @@ const calc = (function() {
   const invert = function(a, b) {
     return a * -1;
   };
+
+  const numToString = function(clicked_id) {
+    inputString += clicked_id;
+    console.log(inputString);
+    document.querySelector(".display").innerHTML = inputString;
+  };
+
+  const operToString = function(clicked_id) {
+    inputString += " " + clicked_id + " ";
+    console.log(inputString);
+    document.querySelector(".display").innerHTML = inputString;
+  };
+
+  const equalButton = function() {
+    input = inputString.split(" ");
+    console.log(input);
+    let a = parseFloat(input[0]);
+    let b = parseFloat(input[2]);
+
+    
+
+  }
 })
